3447e0e50de5e699163b627b65f785fa59ceec8b
Mauro Ezequiel Paris <mauroparis96@gmail.com>
2020-02-23 23:11:36 -0300

    add cache and cors

--- twitter-trends.go
+++ twitter-trends.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
-	"encoding/json"
+	"sort"
+	"time"
 
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
-	"github.com/tkanos/gonfig"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/patrickmn/go-cache"
+	"github.com/tkanos/gonfig"
 )
 
 type Configuration struct {
@@ -25,9 +29,12 @@ type custom_trend struct {
 	TweetVolume int64 `json:"tweet_volume"`
 }
 
+var (
+	cach = cache.New(5*time.Minute, 10*time.Minute)
+	configuration = Configuration{}
+)
+
 func getTwitterClient() *twitter.Client {
-	configuration := Configuration{}
-	gonfig.GetConf("./config.json", &configuration)
 	config := oauth1.NewConfig(
 		configuration.APIKey,
 		configuration.APISecret,
@@ -43,25 +50,31 @@ func getTwitterClient() *twitter.Client {
 }
 
 func getTrends() []custom_trend {
-	configuration := Configuration{}
-	gonfig.GetConf("./config.json", &configuration)
-	client := getTwitterClient()
-	var arg_woeid int64 = configuration.WOEID
+	var cus_trends []custom_trend
+ 	cache_trends, found := cach.Get("cus_trends")
+	
+	 if found {
+		cus_trends = cache_trends.([]custom_trend)
+	} else {
+		client := getTwitterClient()
+		var arg_woeid int64 = configuration.WOEID
+		cus_trends = make([]custom_trend, 0)
+		trends, _, _ := client.Trends.Place(
+			arg_woeid,
+			&twitter.TrendsPlaceParams{},
+		)
 
-	trends, _, _ := client.Trends.Place(
-		arg_woeid,
-		&twitter.TrendsPlaceParams{},
-	)
-
-	var cus_trends = make([]custom_trend, 0)
-
-	for _, v := range trends[0].Trends {
-		if v.TweetVolume > 0 {
-			pop_trend := custom_trend{ v.Name, v.URL, v.TweetVolume }
-			cus_trends = append(cus_trends, pop_trend)
+		for _, v := range trends[0].Trends {
+			if v.TweetVolume > 0 {
+				pop_trend := custom_trend{ v.Name, v.URL, v.TweetVolume }
+				cus_trends = append(cus_trends, pop_trend)
+			}
 		}
+		sort.Slice(cus_trends, func(i, j int) bool {
+			return cus_trends[i].TweetVolume > cus_trends[j].TweetVolume
+		})
+		cach.Set("cus_trends", cus_trends, cache.DefaultExpiration)
 	}
-
 	return cus_trends
 }
 
@@ -73,7 +86,17 @@ func get(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    r := mux.NewRouter()
-    r.HandleFunc("/", get).Methods(http.MethodGet)
-    log.Fatal(http.ListenAndServe(":8080", r))
+	gonfig.GetConf("./config.json", &configuration)
+	r := mux.NewRouter()
+	r.HandleFunc("/", get).Methods(http.MethodGet)
+
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	originsOk := handlers.AllowedOrigins([]string{`*`})
+	methodsOk := handlers.AllowedMethods(
+		[]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"},
+	)
+    log.Fatal(http.ListenAndServe(
+		":8080",
+		handlers.CORS(originsOk, headersOk, methodsOk)(r)),
+	)
 }
